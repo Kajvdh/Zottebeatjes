d2c9eaf4e2cab6b2433dd6ee2b18d5d9a4b87ba9
Kaj <Kaj@Kajtop>
2013-05-05 15:07:15 +0200

    Bij het plaatsen van posts of topic wordt er gecontroleerd of de gebruiker over de benodigde rechten hiervoor beschikt

--- post.php
+++ post.php
@@ -11,6 +11,10 @@ if ($login->getSession()) {
     $member->getById($login->getSession());
     $smarty->assign('login',$member->getUsername());
 }
+else {
+    $member = new Member($db);
+    $member->isGuest(true); //Memberobject als gast aanmaken
+}
 
 $smarty->display('header.tpl');
 echo PHP_EOL;
@@ -19,103 +23,117 @@ echo PHP_EOL;
 $login = new Login();
 if (!$login->getSession()) {
     // niet ingelogd
-    $smarty->display('notloggedin.tpl');
+    $smarty->display('not_logged_in.tpl');
 }
 else {
     if (isset($_POST['newtopic'])) {
         //Form om een nieuw topic te maken is ingevuld
         
-        $errorArr = array();
-        
-        if ((!isset($_POST['forumid']))) {
-            array_push($errorArr,"Het forum waar je een topic probeert te plaatsen bestaat niet.");
-        }
-        else {
-            $forum = new Forum($db);
-            if(!$forum->getById($_POST['forumid'])) {
-                array_push($errorArr,"Het forum waar je een topic probeert te plaatsen bestaat niet.");
-            }
-        }
-
-        if ((!isset($_POST['topicname'])) || (strlen($_POST['topicname']) < "3")) {
-            array_push($errorArr,"Je hebt geen geldige topictitel opgegeven.");
-        }
-        if ((!isset($_POST['post'])) || (strlen($_POST['post']) < "1")) {
-            array_push($errorArr,"Je hebt geen post ingevuld.");
-        }
-
-        if (count($errorArr) > "0") {
-            $smarty->assign('errors',$errorArr);
+        if (!$member->getPermissions()->canPostTopic()) {
+            //Gebruiker heeft geen rechten om topics te starten
+            $smarty->assign('errors',array("Je hebt niet genoeg rechten om een nieuw topic aan te maken."));
             $smarty->display('error.tpl');
         }
         else {
-            //Juist ingevuld, post in de database wegschrijven
-            $post = new Post($db);
-            $topic = new Topic($db);
-            /**
-             * @todo: uitlezen wie de poster is uit de sessie
-             */
-            $author = $login->getSession();
-
-            $topic->setForum($_POST['forumid']);
-            $topic->setTitle($_POST['topicname']);
-            $topic->save();
-
-            $post->setTopic($topic->getId());
-            $post->setAuthor($author);
-            $post->setContent($_POST['post']);
-            $post->setIsNewTopic(true);
-            $post->save();
-            
-            $member = new Member($db);
-            $member->getById($author);
-            $member->incPosts();
-            header("location:board.php?t=".$topic->getId());
-        }
         
+            $errorArr = array();
+
+            if ((!isset($_POST['forumid']))) {
+                array_push($errorArr,"Het forum waar je een topic probeert te plaatsen bestaat niet.");
+            }
+            else {
+                $forum = new Forum($db);
+                if(!$forum->getById($_POST['forumid'])) {
+                    array_push($errorArr,"Het forum waar je een topic probeert te plaatsen bestaat niet.");
+                }
+            }
+
+            if ((!isset($_POST['topicname'])) || (strlen($_POST['topicname']) < "3")) {
+                array_push($errorArr,"Je hebt geen geldige topictitel opgegeven.");
+            }
+            if ((!isset($_POST['post'])) || (strlen($_POST['post']) < "1")) {
+                array_push($errorArr,"Je hebt geen post ingevuld.");
+            }
+
+            if (count($errorArr) > "0") {
+                $smarty->assign('errors',$errorArr);
+                $smarty->display('error.tpl');
+            }
+            else {
+                //Juist ingevuld, post in de database wegschrijven
+                $post = new Post($db);
+                $topic = new Topic($db);
+                /**
+                 * @todo: uitlezen wie de poster is uit de sessie
+                 */
+                $author = $login->getSession();
+
+                $topic->setForum($_POST['forumid']);
+                $topic->setTitle($_POST['topicname']);
+                $topic->save();
+
+                $post->setTopic($topic->getId());
+                $post->setAuthor($author);
+                $post->setContent($_POST['post']);
+                $post->setIsNewTopic(true);
+                $post->save();
+
+                $member = new Member($db);
+                $member->getById($author);
+                $member->incPosts();
+                header("location:board.php?t=".$topic->getId());
+            }
+        }
     }
     elseif (isset($_POST['newpost'])) {
         //Form om een nieuwe post te plaatsen in een bestaand topic is ingevuld
-        
-        $errorArr = array();
-        
-        if ((!isset($_POST['topicid']))) {
-            array_push($errorArr,"Het topic waar je een reactie probeert te plaatsen bestaat niet.");
-        }
-        else {
-            $topic = new Topic($db);
-            if (!$topic->getById($_POST['topicid'])) {
-                array_push($errorArr,"Het topic waar je een reactie probeert te plaatsen bestaat niet.");
-            }
-        }
-        
-        if ((!isset($_POST['post'])) || (strlen($_POST['post']) < "1")) {
-            array_push($errorArr,"Je hebt geen post ingevuld.");
-        }
-
-        if (count($errorArr) > "0") {
-            $smarty->assign('errors',$errorArr);
+        if (!$member->getPermissions()->canPostReply()) {
+            //Gebruiker heeft geen rechten om te reageren op topics
+            $smarty->assign('errors',array("Je hebt niet genoeg rechten te reageren op topics."));
             $smarty->display('error.tpl');
         }
         else {
-            //Juist ingevuld, post in de database wegschrijven
-            $post = new Post($db);
-            /**
-             * @todo: uitlezen wie de poster is uit de sessie
-             */
-            $author = $login->getSession();
-            
-            $post->setTopic($_POST['topicid']);
-            $post->setAuthor($author);
-            $post->setContent($_POST['post']);
-            $post->setIsNewTopic(false);
-            $post->save();
-            
-            $member = new Member($db);
-            $member->getById($author);
-            $member->incPosts();
-            header("location:board.php?t=".$post->getTopic());
+            $errorArr = array();
+        
+            if ((!isset($_POST['topicid']))) {
+                array_push($errorArr,"Het topic waar je een reactie probeert te plaatsen bestaat niet.");
+            }
+            else {
+                $topic = new Topic($db);
+                if (!$topic->getById($_POST['topicid'])) {
+                    array_push($errorArr,"Het topic waar je een reactie probeert te plaatsen bestaat niet.");
+                }
+            }
+
+            if ((!isset($_POST['post'])) || (strlen($_POST['post']) < "1")) {
+                array_push($errorArr,"Je hebt geen post ingevuld.");
+            }
+
+            if (count($errorArr) > "0") {
+                $smarty->assign('errors',$errorArr);
+                $smarty->display('error.tpl');
+            }
+            else {
+                //Juist ingevuld, post in de database wegschrijven
+                $post = new Post($db);
+                /**
+                 * @todo: uitlezen wie de poster is uit de sessie
+                 */
+                $author = $login->getSession();
+
+                $post->setTopic($_POST['topicid']);
+                $post->setAuthor($author);
+                $post->setContent($_POST['post']);
+                $post->setIsNewTopic(false);
+                $post->save();
+
+                $member = new Member($db);
+                $member->getById($author);
+                $member->incPosts();
+                header("location:board.php?t=".$post->getTopic());
+            }
         }
+        
     }
     else {
         //nog geen post geplaatst
@@ -127,13 +145,27 @@ else {
         
         if (isset($_GET['t'])) {
             //Reageren op een topic
-            $smarty->assign('topicId',$_GET['t']);
-            $smarty->display('newpost.tpl');
+            if (!$member->getPermissions()->canPostReply()) {
+                //Gebruiker heeft geen rechten om te reageren op topics
+                $smarty->assign('errors',array("Je hebt niet genoeg rechten te reageren op topics."));
+                $smarty->display('error.tpl');
+            }
+            else {
+                $smarty->assign('topicId',$_GET['t']);
+                $smarty->display('newpost.tpl');
+            }
         }
         elseif (isset($_GET['f'])) {
             //Nieuw topic aanmaken
-            $smarty->assign('forumId',$_GET['f']);
-            $smarty->display('newtopic.tpl');
+            if (!$member->getPermissions()->canPostTopic()) {
+                //Gebruiker heeft geen rechten om topics te starten
+                $smarty->assign('errors',array("Je hebt niet genoeg rechten om een nieuw topic aan te maken."));
+                $smarty->display('error.tpl');
+            }
+            else {
+                $smarty->assign('forumId',$_GET['f']);
+                $smarty->display('newtopic.tpl');
+            }
         }
         else {
             $errorArr = array();
