0603151382182736853ebc115c46a6ec485dcef6
Michael <deboeure.s@hotmail.com>
2013-05-10 17:49:54 +0200

    Toevoeging van classeVote, om de individuele votes te verwerken. Aanpassen van de html voor het weergeven van de webpoll

---
+++ classes/Vote.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Vote
+ *
+ * @author Michael
+ */
+class Vote {
+    private $_db;
+    
+    public $id;
+    public $poll;
+    public $answer;
+    public $member;
+    
+    public function _construct(PDO $db) {
+        $this->_db = $db;
+    }
+    
+    public function setPoll($pollId) {
+        $this->poll = $pollId;
+    }
+    
+    public function setAnswer($answerId) {
+        $this->answer = $answerId;
+    }
+    
+    public function setMember($memberId) {
+        $this->member = $memberId;
+    }
+    
+    public function getId() {
+        return $this->poll;
+    }
+    
+    public function getPoll() {
+        return $this->id;
+    }
+    
+    public function getAnswer() {
+        return $this->answer;
+    }
+    
+    public function getMember() {
+        return $this->member;
+    }
+    
+    public function available() {
+        return true;
+    }
+    
+    public function save() {
+        if (!$this->available()) {
+            return false;
+        }
+        else {
+            $qry = $this->_db->prepare("INSERT INTO votes(poll,answer,member) VALUES(:poll,:answer,:member);");
+            $data = array(
+                ':poll' => $this->poll,
+                ':answer' => $this->answer,
+                ':member' => $this->member,
+            );
+            
+            $qry->execute($data);
+            if ($qry->rowCount() > '0') {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+    
+    public function getById($id) {
+        $this->id = $id;
+        $stmp = $this->_db->prepare("SELECT * FROM `votes` WHERE `id`= ?;");
+        $stmp->execute(array($this->id));
+        $row = $stmp->fetch(PDO::FETCH_ASSOC);
+        
+        $this->id = $row['id'];
+        $this->poll = $row['poll'];
+        $this->answer = $row['answer'];
+        $this->member = $row['member'];
+    }
+}
+
+?>
--- templates/poll.tpl
+++ templates/poll.tpl
@@ -1,10 +1,17 @@
 
              
 {foreach from=$polls item=poll}
+    <form class="webpoll" method="post">
     <h2>{$poll['question']}</h2>
+    <fieldset>
     <ul>
     {foreach from=$poll['answers'] item=answer}
-        <li><a href="?pid={$poll['id']}&aid={$answer['id']}">{$answer['content']}</a></li>
+        <li class='poll_results'>
+            <div class='result' style='width:40px;'>&nbsp;</div>
+            <label>{$answer['content']}</label>
+        </li>
     {/foreach}
     </ul>
+    </fieldset>
+    </form> 
 {/foreach}
